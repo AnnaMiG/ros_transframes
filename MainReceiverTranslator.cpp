e2add8390e55f1306da23e95534b5ea33f6eec13
AnnaMiG <43910226+AnnaMiG@users.noreply.github.com>
2018-12-12 11:44:42 +0000

    Update MainReceiverTranslator.cpp

--- MainReceiverTranslator.cpp
+++ MainReceiverTranslator.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "tf_receiver_translator");
     ros::NodeHandle n;
     ros::Rate rate(1.0);
-    ReceiverTranslator tr_Obj(ros::Time::now());
+    ReceiverTranslator tr_Obj();
     /// the transframes tf_ned and tf_nwu will be published every second
     while(n.ok())
     {
